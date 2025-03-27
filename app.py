ce28f8b826f5b2471cadaf6d5b44f7d0bac97005
Erin DeLong <erindelong@umail.ucsb.edu>
2025-03-27 16:13:50 -0700

    added key

--- app.py
+++ app.py
@@ -144,6 +144,7 @@ def require_api_key(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         api_key = request.headers.get('X-HR-KEY')
+        print("api key: ", api_key)
         if api_key != API_KEY:
             return jsonify({"error": "Unauthorized"}), 401
         return f(*args, **kwargs)
